76725ee653c7b26a1a0b4eafd301f85f574ed70c
Guilherme <silva.correa@aluno.ifsp.edu.br>
2024-09-19 11:30:52 -0300

    Primeira modificacao

---
+++ banco/Twitter.sql
@@ -0,0 +1,23 @@
+create database Twitter2;
+
+USE database Twitter2;
+
+create table Usuario(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    email VARCHAR(100),
+    senha CHAR(60),
+    dataCriacao datetime,
+    ativo tinyint(4),
+    adm tinyint(4)
+);
+
+create table Post(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(100),
+    texto text,
+    dataCriacao datetime,
+    dataPostagem datetime,
+    idUsuario INT,
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
+)
---
+++ includes/funcoes.php
@@ -0,0 +1,5 @@
+<?php
+    function limparDados(string $dado) : string{
+        $tags =  
+    }
+?>
